19dfcca0a9fde9efb6cea7b3415741cc451c09c2
Atamak105 <yk20195419@gmail.com>
2020-07-31 11:07:04 +0900

    フロントサイド既読機能実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+
   def index  # indexアクションを定義した
     @posts = Post.all.order(id: "DESC") #1番目のレコードを@postに代入
   end
@@ -20,5 +21,7 @@ class PostsController < ApplicationController
     item = Post.find(params[:id])
     render json: { post: item }
   end
+
+
 end
 
